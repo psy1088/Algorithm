cd78d4f951c959ca64f2eac8c8472887d7cae7f8
psy6190 <psy6190@gmail.com>
2021-02-15 00:03:02 +0900

    dfs bfs 7

--- practice/dfs,bfs7.py
+++ practice/dfs,bfs7.py
@@ -1,16 +1,10 @@
+# p353 인구 이동
 from collections import deque
 
 # N, L, R = 3, 5, 10  # N=맵크기, 국경선 오픈조건= L <= x <= R
 # game_map = [[10, 15, 20], [20, 30, 25], [40, 22, 10]]
 N, L, R = 4, 10, 50  # N=맵크기, 국경선 오픈조건= L <= x <= R
 game_map = [[10, 100, 20, 90], [80, 100, 60, 70], [70, 20, 30, 40], [50, 20, 100, 10]]
-
-dr = [-1, 0, 1, 0]
-dc = [0, -1, 0, 1]
-visited = [[0] * N for _ in range(N)]
-move_cnt = 0
-q = deque()
-group = deque()
 
 
 def bfs(i, j):
@@ -29,7 +23,7 @@ def bfs(i, j):
                 if L <= diff <= R:  # 인접 국가의 차이가 L이상 R이하라면, 해당 국가 체크
                     visited[n_r][n_c] = 1
                     q.append((n_r, n_c))
-                    group.append((n_r, n_c))
+                    group.append((n_r, n_c))  # 그룹에 추가
                     total += game_map[n_r][n_c]
 
     average = total // len(group)
@@ -39,10 +33,15 @@ def bfs(i, j):
 # # def bfs() 끝 # #
 
 
+dr = [-1, 0, 1, 0]
+dc = [0, -1, 0, 1]
+move_cnt = 0
+q = deque()
+group = deque()
+
 while True:
     visited = [[0] * N for _ in range(N)]
     index = 0  # 국경선을 공유한 국가가 있는지 알아보기 위함
-
     for i in range(N):
         for j in range(N):
             group.clear()
@@ -51,6 +50,7 @@ while True:
                 index += 1
     if index == N * N:  # 국경선을 공유한 국가가 하나도 없는 경우 break
         break
+
     move_cnt += 1
 
 print(move_cnt)
