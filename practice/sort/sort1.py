abc259524297292badb3195284b52c90239435f1
psy6190 <psy6190@gmail.com>
2021-02-16 23:13:03 +0900

    sort1

---
+++ practice/sort/sort1.py
@@ -0,0 +1,11 @@
+# p359 국영수
+N = 12
+student = [["Junkyu", 50, 60, 100], ["Sangkeun", 80, 60, 50], ["Sunyuong", 80, 70, 100], ["Soong", 50, 60, 90],
+           ["Haebin", 50, 60, 100], ["Kangsoo", 60, 80, 100], ["Donghyuk", 80, 60, 100], ["Sei", 70, 70, 70],
+           ["Wonseb", 70, 70, 90], ["Sanghyun", 70, 70, 80], ["nsj", 80, 80, 80], ["Taewhan", 50, 60, 90]]
+
+# 다중조건 정렬
+student.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for i in student:
+    print(i[0])
