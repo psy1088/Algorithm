52513db91f0c38a2f6cd62b1b5e127f0bc3900c9
psy6190 <psy6190@gmail.com>
2021-02-19 13:18:56 +0900

    search2

---
+++ practice/Search/search2.py
@@ -0,0 +1,19 @@
+# p368 고정점 찾기
+N = 7  # 리스트의 개수
+data = [-15, -4, 2, 8, 9, 13, 15]
+
+
+def fix_point(arr, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if mid == arr[mid]:
+            return mid
+        elif mid < data[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+
+
+print(fix_point(data, 0, N-1))
