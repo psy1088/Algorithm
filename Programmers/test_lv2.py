6b368beff2c6563398bf9c909dd22dd42113dd6b
psy6190 <psy6190@gmail.com>
2021-03-12 02:46:51 +0900

    .

--- Programmers/test_lv2.py
+++ Programmers/test_lv2.py
@@ -1,24 +1,12 @@
 def solution(number, k):
-    answer = ''
-    temp = []
-    len_number = len(number)
+    stack = [number[0]]
+    for num in number[1:]:
+        while stack and num > stack[-1] and k > 0:
+            stack.pop()
+            k -= 1
+        stack.append(num)
 
-    for i in range(len_number):
-        temp.append((number[i], i))
+    for i in range(k):  # 최댓값을 구했는데, k가 남았다면, 남은 수만큼 pop
+        stack.pop()
 
-    temp.sort(key=lambda x: (-int(x[0]), x[1]))  # 값에 대하여 내림차순, 값이 같다면 인덱스 순으로 오름차순
-
-    remain_str = len_number - k
-    last_index = 0
-    while len(answer) < len_number - k:  # 문자열이 완성될 때까지 반복
-        for t in temp:
-            if t[1] + remain_str <= len_number and t[1] > last_index:
-                answer += t[0]  # 문자열에 추가
-                remain_str -= 1  # 남은 문자길이 감소
-                last_index = t[1]  # 현재까지 완료된 문자열의 마지막 인덱스를 저장
-                temp.remove(t)
-                break
-    return answer
-
-
-print(solution("1231234", 3))
+    return ''.join(stack)
