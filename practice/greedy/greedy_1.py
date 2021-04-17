7cfacbaa1e7c3bd7f354d85cfb147a8329cad2a6
psy6190 <psy6190@gmail.com>
2021-04-17 11:15:49 +0900

    수정

--- practice/greedy/greedy_1.py
+++ practice/greedy/greedy_1.py
@@ -3,17 +3,18 @@
 num = int(input()) # 인원 수 입력
 data = list(map(int, input().split())) # 인원의 공포수치 입력
 
-data.sort() # 리스트 오름차순 정렬
+data.sort(reverse=True) # 리스트 오름차순 정렬
 
 total_cnt = 0 # 총 그룹 수
 cur_cnt = 0 # 현재 그룹 수
 
-# 현재 그룹을 결성한 멤버의 공포도가 그 그룹의 현재 인원보다 작거나 같다면 그룹 결성
-for i in data:
+start = data[0]
+for i in range(num):
     cur_cnt += 1
-    if cur_cnt >= i:
+    if cur_cnt >= start:
         total_cnt += 1
-        print(cur_cnt)
         cur_cnt = 0
+        if i+1 < num:
+            start = data[i+1]
 
 print("최대그룹 수 : ", total_cnt)
