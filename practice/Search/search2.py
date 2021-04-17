be2bf7e8e2c4085f5de3694c31ae8c9187061adb
psy6190 <psy6190@gmail.com>
2021-04-17 11:51:23 +0900

    수정

--- practice/Search/search2.py
+++ practice/Search/search2.py
@@ -3,16 +3,22 @@ N = 7  # 리스트의 개수
 data = [-15, -4, 2, 8, 9, 13, 15]
 
 
-def fix_point(arr, start, end):
-    while start <= end:
-        mid = (start + end) // 2
+# def fix_point(arr, start, end):
+#     while start <= end:
+#         mid = (start + end) // 2
+#
+#         if mid == arr[mid]:
+#             return mid
+#         elif mid < data[mid]:
+#             end = mid - 1
+#         else:
+#             start = mid + 1
+#     return -1
 
-        if mid == arr[mid]:
-            return mid
-        elif mid < data[mid]:
-            end = mid - 1
-        else:
-            start = mid + 1
+def fix_point(arr, start, end):
+    for val, index in enumerate(data):
+        if val == index:
+            return val
     return -1
 
 
